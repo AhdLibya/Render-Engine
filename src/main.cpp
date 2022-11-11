35eb8c6fe6b481d518ff55b02eba3ab18594f2bd
Ahd libya <97837394+AhdLibya@users.noreply.github.com>
2022-11-11 06:49:03 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vulkan/vulkan.h>
+
+
+using namespace std;
+
+
+int main() {
+
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "AHD";
+    appInfo.pEngineName = "AHD Engine";
+
+    VkInstanceCreateInfo instanceInfo = {};
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pApplicationInfo = &appInfo;
+
+    VkInstance Instance;
+
+    VkResult result = vkCreateInstance(&instanceInfo , nullptr , &Instance);
+    
+    if(result == VK_SUCCESS)
+    {
+        cout<< "Instance Created Successfully"<< endl;
+    }
+
+    return 0;
+}
